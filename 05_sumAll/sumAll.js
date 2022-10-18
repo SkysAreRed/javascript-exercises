89a843e14e399c367825008f3eee9624ecd7a257
SkysAreRed <skysarered2@gmail.com>
2022-10-18 15:28:26 +0200

    Finished tasks 1 through 7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
-
-};
-
-// Do not edit below this line
-module.exports = sumAll;
+const sumAll = function(min, max) {
+    if (min < 0 || max < 0) return "ERROR"; // returns error message if and negative numbers are input
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR"; // determines numbers value and ! reverses result
+    if (min > max) { // copies min into temporary, max to min, and moves temp to the max var. aka ordering from S to L
+      const temp = min;
+      min = max;
+      max = temp;
+    }
+    let sum = 0;
+    for (let i = min; i < max + 1; i++) {
+      sum += i;
+    }
+    return sum;
+  };
+  
+  module.exports = sumAll;
